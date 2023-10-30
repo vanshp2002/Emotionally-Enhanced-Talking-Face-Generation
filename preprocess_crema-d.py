8fc8eeaa47c44686ef42059c342aa2b4537a2af4
Harsh <hs0033947@gmail.com>
2023-10-30 19:02:12 +0530

    Added preprocessing functions

--- preprocess_crema-d.py
+++ preprocess_crema-d.py
@@ -5,7 +5,11 @@ from os import listdir, path
 import multiprocessing as mp
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import numpy as np
-import argparse, os, cv2, traceback, subprocess
+import argparse
+import os
+import cv2
+import traceback
+import subprocess
 from tqdm import tqdm
 from glob import glob
 from hparams import hparams as hp
@@ -14,44 +18,100 @@ import face_detection
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--ngpu', help='Number of GPUs available', default=1, type=int)
+parser.add_argument(
+    '--ngpu', help='Number of GPUs available', default=1, type=int)
 parser.add_argument('--batch_size', help='batch size', default=32, type=int)
-parser.add_argument("--data_root", help="Root folder of the dataset", required=True)
-parser.add_argument("--preprocessed_root", help="Root folder of the preprocessed dataset", required=True)
+parser.add_argument(
+    "--data_root", help="Root folder of the dataset", required=True)
+parser.add_argument("--preprocessed_root",
+                    help="Root folder of the preprocessed dataset", required=True)
 
 args = parser.parse_args()
 
-fa = [face_detection.FaceAlignment(face_detection.LandmarksType._2D, flip_input=False, 
-									device='cuda:{}'.format(id)) for id in range(args.ngpu)]
+fa = [face_detection.FaceAlignment(face_detection.LandmarksType._2D, flip_input=False,
+                                   device='cuda:{}'.format(id)) for id in range(args.ngpu)]
+
+
+template = 'ffmpeg -loglevel panic -y -i {} -strict -2 {}'
 
 
 def process_video_file(vfile, args, gpu_id):
-	video_stream = cv2.VideoCapture(vfile)
-	
-	frames = []
-	while 1:
-		still_reading, frame = video_stream.read()
-		if not still_reading:
-			video_stream.release()
-			break
-		frames.append(frame)
-	
-	vidname = os.path.basename(vfile).split('.')[0]
+    video_stream = cv2.VideoCapture(vfile)
 
-	fulldir = path.join(args.preprocessed_root, vidname)
-	os.makedirs(fulldir, exist_ok=True)
+    frames = []
+    while 1:
+        still_reading, frame = video_stream.read()
+        if not still_reading:
+            video_stream.release()
+            break
+        frames.append(frame)
 
-	batches = [frames[i:i + args.batch_size] for i in range(0, len(frames), args.batch_size)]
+    vidname = os.path.basename(vfile).split('.')[0]
 
-	i = -1
-	for fb in batches:
-		preds = fa[gpu_id].get_detections_for_batch(np.asarray(fb))
+    fulldir = path.join(args.preprocessed_root, vidname)
+    os.makedirs(fulldir, exist_ok=True)
 
-		for j, f in enumerate(preds):
-			i += 1
-			if f is None:
-				continue
+    batches = [frames[i:i + args.batch_size]
+               for i in range(0, len(frames), args.batch_size)]
 
-			x1, y1, x2, y2 = f
-			cv2.imwrite(path.join(fulldir, '{}.jpg'.format(i)), fb[j][y1:y2, x1:x2])
-		
+    i = -1
+    for fb in batches:
+        preds = fa[gpu_id].get_detections_for_batch(np.asarray(fb))
+
+        for j, f in enumerate(preds):
+            i += 1
+            if f is None:
+                continue
+
+            x1, y1, x2, y2 = f
+            cv2.imwrite(path.join(fulldir, '{}.jpg'.format(i)),
+                        fb[j][y1:y2, x1:x2])
+
+
+def process_audio_file(vfile, args):
+    vidname = os.path.basename(vfile).split('.')[0]
+
+    fulldir = path.join(args.preprocessed_root, vidname)
+    os.makedirs(fulldir, exist_ok=True)
+
+    wavpath = path.join(fulldir, 'audio.wav')
+
+    command = template.format(vfile, wavpath)
+    subprocess.call(command, shell=True)
+
+
+def mp_handler(job):
+    vfile, args, gpu_id = job
+    try:
+        process_video_file(vfile, args, gpu_id)
+    except KeyboardInterrupt:
+        exit(0)
+    except:
+        traceback.print_exc()
+
+
+def main(args):
+    print('Started processing for {} with {} GPUs'.format(
+        args.data_root, args.ngpu))
+
+    filelist = glob(path.join(args.data_root, '*.mp4'))
+
+    jobs = [(vfile, args, i % args.ngpu) for i, vfile in enumerate(filelist)]
+    p = ThreadPoolExecutor(args.ngpu)
+    futures = [p.submit(mp_handler, j) for j in jobs]
+    _ = [r.result() for r in tqdm(as_completed(futures), total=len(futures))]
+
+    print('Dumping audios...')
+
+    for vfile in tqdm(filelist):
+        try:
+            process_audio_file(vfile, args)
+        except KeyboardInterrupt:
+            exit(0)
+        except:
+            traceback.print_exc()
+            continue
+
+
+if __name__ == '__main__':
+    main(args)
