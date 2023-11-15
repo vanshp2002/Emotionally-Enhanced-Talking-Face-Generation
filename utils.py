d287377fd81a3ec0d92f23a9f63c2527468058d2
Fenil295 <fenildalwala999@gmail.com>
2023-11-15 23:07:22 +0530

    Modified

--- utils.py
+++ utils.py
@@ -19,3 +19,13 @@ class perceptionLoss():
             if name in self.feature_layers:
                 feature_list.append(x)
         return feature_list
+    
+    def calculatePerceptionLoss(self, video_pd, video_gt):
+        features_pd = self.getfeatures(video_pd.view(video_pd.size(0)*video_pd.size(2), video_pd.size(1), video_pd.size(3), video_pd.size(4)))
+        features_gt = self.getfeatures(video_gt.view(video_gt.size(0)*video_gt.size(2), video_gt.size(1), video_gt.size(3), video_gt.size(4)))
+        
+        with torch.no_grad():
+            features_gt = [x.detach() for x in features_gt]
+        
+        perceptual_loss = sum([self.mse_loss(features_pd[i], features_gt[i]) for i in range(len(features_gt))])
+        return perceptual_loss
